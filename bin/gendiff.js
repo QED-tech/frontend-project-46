a8cfd2864ba0f0920d71f15d0119035eb3b42423
qed-tech <qedirast@gmail.com>
2023-03-24 18:08:55 +0700

    added arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,13 @@
 const { Command } = require('commander');
+
 const program = new Command();
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0', '-V, --version', 'output the version number');
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse();
