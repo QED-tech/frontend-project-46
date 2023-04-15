7684770777ac5e171003717d82857eeaa2877066
qed-tech <qedirast@gmail.com>
2023-04-15 21:08:48 +0700

    implemented plain formatter

---
+++ src/formatters/formatter.js
@@ -0,0 +1,36 @@
+import lodash from 'lodash';
+
+const levelSpaces = '  ';
+
+const getDiffRowByNode = (node, nodeKey) => {
+  switch (node.state) {
+    case 'equals': {
+      return `${levelSpaces}  ${nodeKey}: ${node.value}`;
+    }
+    case 'deleted': {
+      return `${levelSpaces}- ${nodeKey}: ${node.value}`;
+    }
+    case 'added': {
+      return `${levelSpaces}+ ${nodeKey}: ${node.value}`;
+    }
+    case 'changed': {
+      const rowWithOldValue = `${levelSpaces}- ${nodeKey}: ${node.old_value}`;
+      const rowWithNewValue = `${levelSpaces}- ${nodeKey}: ${node.new_value}`;
+
+      return [rowWithOldValue, rowWithNewValue].join('\n');
+    }
+
+    default: {
+      return nodeKey;
+    }
+  }
+};
+
+export default (ast, formatForPresent) => {
+  const build = lodash.reduce(ast, (acc, val, key) => {
+    acc.push(getDiffRowByNode(val, key));
+    return acc;
+  }, []);
+
+  return ['{', build.join('\n'), '}'].join('\n');
+};
