f87e242e748be04d775dd1088de5265a8d33833a
qed-tech <qedirast@gmail.com>
2023-04-21 18:18:19 +0700

    fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import fs from 'fs';
 
-import genDiff from '../src/differ.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
